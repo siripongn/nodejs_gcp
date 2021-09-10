043bf068523097d92f8988e3f9f737b6545e2dbc
siripongn <siripong.ut.n@gmail.com>
2021-09-11 05:50:16 +0700

    1-st

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 4444;
+
+app.get('/',(req,res)=>{
+    res.send('<h1> TESTING</h1>  <h3> Message: Success</h3> <h4> Ver. 1.0 <h4>');
+})
+
+app.get('/more',(req,res)=>{
+    res.send([
+        {
+        testId:'001',
+        price:99
+    },
+    {
+        testId:'002',
+        price:88
+    }
+])
+
+})
+
+app.listen(port,()=>{
+    console.log(`TEST ON PORT ${port}`);
+})
